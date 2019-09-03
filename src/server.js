41242c6fcb3f6ee66b1e6291c301ace0a63e06e1
Victor Vuelma <victorvuelma@outlook.com>
2019-09-03 00:12:09 -0300

    fix cors

--- src/server.js
+++ src/server.js
@@ -3,18 +3,28 @@ const socket = require('socket.io')
 const dotenv = require('dotenv')
 dotenv.config()
 
-const server = require('http').createServer()
+const server = require('http').createServer((req, res) => {
+  // Set CORS headers
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Request-Method', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET')
+  res.setHeader('Access-Control-Allow-Headers', '*')
+  if (req.method === 'OPTIONS') {
+    res.writeHead(200)
+    res.end()
+    return
+  }
+
+  // ...
+})
 const io = socket(server)
 
 const mqtt = require('mqtt')
-const mqttClient = mqtt.connect(
-  `mqtt://${process.env.MQTT_HOST}`,
-  {
-    port: process.env.MQTT_PORT,
-    username: process.env.MQTT_USER,
-    password: process.env.MQTT_PASS
-  }
-)
+const mqttClient = mqtt.connect(`mqtt://${process.env.MQTT_HOST}`, {
+  port: process.env.MQTT_PORT,
+  username: process.env.MQTT_USER,
+  password: process.env.MQTT_PASS,
+})
 
 io.on('connection', client => {
   client.on('connectTraffic', slug => {
