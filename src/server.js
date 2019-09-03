8309580750aaf82658126c99432b49e28e75be8e
Victor Vuelma <victorvuelma@outlook.com>
2019-09-02 23:54:45 -0300

    init project

---
+++ src/server.js
@@ -0,0 +1,39 @@
+const socket = require('socket.io')
+
+const dotenv = require('dotenv')
+dotenv.config()
+
+const server = require('http').createServer()
+const io = socket(server)
+
+const mqtt = require('mqtt')
+const mqttClient = mqtt.connect(
+  `mqtt://${process.env.MQTT_HOST}`,
+  {
+    port: process.env.MQTT_PORT,
+    username: process.env.MQTT_USER,
+    password: process.env.MQTT_PASS
+  }
+)
+
+io.on('connection', client => {
+  client.on('connectTraffic', slug => {
+    client.join(`tl_${slug}`)
+  })
+})
+
+mqttClient.subscribe('st/traffic_light')
+
+mqttClient.on('message', (topic, message, packet) => {
+  const base64 = Buffer.from(message).toString('ascii')
+  const buff = Buffer.from(base64, 'base64').toString('ascii')
+  const payload = JSON.parse(buff.toString('ascii'))
+
+  switch (topic) {
+    case 'st/traffic_light':
+      io.sockets.in(`tl_${payload.slug}`).emit(payload.action, payload.state)
+      break
+  }
+})
+
+server.listen(process.env.PORT || 8080)
