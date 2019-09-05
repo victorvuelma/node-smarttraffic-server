cb8e41e289e40b41d9ee9ae3cb1089413ddeb217
Victor Vuelma <victorvuelma@outlook.com>
2019-09-04 23:01:21 -0300

    fix on client connect dont send state

--- src/server.js
+++ src/server.js
@@ -17,18 +17,16 @@ const server = require('http').createServer((req, res) => {
 
   // ...
 })
-const io = socket(server)
 
-const mqtt = require('mqtt')
-const mqttClient = mqtt.connect(`mqtt://${process.env.MQTT_HOST}`, {
-  port: process.env.MQTT_PORT,
-  username: process.env.MQTT_USER,
-  password: process.env.MQTT_PASS,
-})
+const states = {}
+
+const io = socket(server)
 
 io.on('connection', client => {
   client.on('connectTraffic', slug => {
     client.join(`tl_${slug}`)
+
+    client.emit('change_state', states[slug] || '')
   })
 
   client.on('alterState', data => {
@@ -39,8 +37,12 @@ io.on('connection', client => {
   })
 })
 
-mqttClient.subscribe('st/traffic_light')
-mqttClient.subscribe('st/cross')
+const mqtt = require('mqtt')
+const mqttClient = mqtt.connect(`mqtt://${process.env.MQTT_HOST}`, {
+  port: process.env.MQTT_PORT,
+  username: process.env.MQTT_USER,
+  password: process.env.MQTT_PASS,
+})
 
 mqttClient.on('message', (topic, message, packet) => {
   const base64 = Buffer.from(message).toString('ascii')
@@ -49,9 +51,15 @@ mqttClient.on('message', (topic, message, packet) => {
 
   switch (topic) {
     case 'st/traffic_light':
+      states[payload.slug] = payload.state
+
       io.sockets.in(`tl_${payload.slug}`).emit(payload.action, payload.state)
+
       break
   }
 })
+
+mqttClient.subscribe('st/traffic_light')
+mqttClient.subscribe('st/cross')
 
 server.listen(process.env.PORT || 8000)
